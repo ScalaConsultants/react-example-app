855b946798caa8d260589b9e72c13fcbf888ded8
Alwox <alanwolejko@gmail.com>
2017-05-15 14:42:09 +0200

    display product details

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Screen from '../../styledComponents/Screen'
+import products from '../../data/products'
 
-const ProductDetails = () => {
+const ProductDetails = (props) => {
+
+  const id = parseInt(props.match.params.id, 10)
+  const product = products.filter(product => product.id === id)[0]
+
   return (
     <Screen>
       <h3>Product Details</h3>
+      <p>product: {product.name}</p>
+      <p>price: {product.price}</p>
     </Screen>
     )
 }
