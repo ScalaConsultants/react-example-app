80fb922529f36704a1aa33bb2c1baa764d3b96f6
Alwox <alanwolejko@gmail.com>
2017-05-15 12:34:52 +0200

    add link button

---
+++ src/components/Navbar/CartButton.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+// language=SCSS prefix=dummy{ suffix=}
+const Logo = styled.span`
+    color: #eaf4ff;
+    font-size: 30px;
+    float:right;
+`;
+
+export default Logo;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 import NavbarStyled from './NavbarStyle';
 import Logo from './Logo';
+import CartButton from './CartButton'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
       <NavbarStyled>
-        <Logo>SHOP NAME</Logo>
+        <Logo><Link to='/'>SHOP NAME</Link></Logo>
+        <CartButton><Link to='/cart'>Cart</Link></CartButton>
       </NavbarStyled>
     )
 }
