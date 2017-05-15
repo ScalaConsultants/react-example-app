d3c9e0ee847725dd02f1695b30e8b29eefa9314e
Alwox <alanwolejko@gmail.com>
2017-05-15 12:58:12 +0200

    add test products data

---
+++ src/data/products.js
@@ -0,0 +1,24 @@
+const products = [
+  {
+    name:'product1',
+    price:12
+  },
+  {
+    name:'product2',
+    price:22
+  },
+  {
+    name:'product3',
+    price:7
+  },
+  {
+    name:'product4',
+    price:34
+  },
+  {
+    name:'product5',
+    price:17
+  }
+]
+
+export default products;
