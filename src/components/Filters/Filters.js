21b741aba305cc346ad79b754de80cc761c0d8a2
Alwox <alanwolejko@gmail.com>
2017-05-16 11:51:33 +0200

    add sort buttons

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import FilterStyle from './FiltersStyle'
+import Button from '../../styledComponents/Button'
 
 const Filters = () => {
     return (
       <FilterStyle>
         <h2>Filters</h2>
+        <p>Sort by price:</p>
+        <Button>Highest</Button>
+        <Button>Lowest</Button>
       </FilterStyle>
     )
 }
