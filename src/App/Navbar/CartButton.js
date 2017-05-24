59c7f0ba8c44fc578ec9208b0e2a8f83ce28559d
Alwox <alanwolejko@gmail.com>
2017-05-24 14:34:57 +0200

    refactoring: merge unnecessary once used styled-components into components (add missed 2)

--- src/App/Navbar/CartButton.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-// language=SCSS prefix=dummy{ suffix=}
-const Logo = styled.img`
-    margin:8px;
-    float:right;
-`
-
-export default Logo
--- src/App/Navbar/Navbar.js
+++ src/App/Navbar/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import Logo from './Logo'
-import CartButton from './CartButton'
 import { Link } from 'react-router-dom'
 
 // language=SCSS prefix=dummy{ suffix=}
@@ -9,6 +7,20 @@ const NavbarStyle = styled.div`
     height: 80px;
     background-color: rgb(24,24,24);
     padding:0 180px;
+`
+
+// language=SCSS prefix=dummy{ suffix=}
+const CartButton = styled.img`
+    margin:8px;
+    float:right;
+`
+
+// language=SCSS prefix=dummy{ suffix=}
+const Logo = styled.span`
+    color: rgb(255, 255, 255);
+    font-size: 46px;
+    float: left;
+    margin: 15px 0;
 `
 
 const Navbar = () => {
