0d3203b72d5c4c43fe09e7243b7bc3451e08f49f
Alwox <alanwolejko@gmail.com>
2017-05-25 10:24:33 +0200

    refactoring: extract presentational component

--- src/containers/ProductDetails/ProductDetails.js
+++ src/containers/ProductDetails/ProductDetails.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import Screen from '../../components/Screen'
-import Button from '../../components/Button'
 import products from '../../lib/data/products'
 import categories from '../../lib/data/categories'
 import {connect} from 'react-redux'
-import { Link } from 'react-router-dom'
 import {addToCart} from '../ShoppingCart/actionCreators'
+import ProductsDetailsView from './ProductDetailsView'
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    addToCart: (product,amount) => dispatch(addToCart(product,amount))
+    addToCart: (product) => dispatch(addToCart(product))
   }
 }
 
@@ -20,21 +18,10 @@ const ProductDetails = (props) => {
   const category = categories.find(category => category.id === product.category).name
 
   return (
-    <Screen>
-      <h3>{product.name}</h3>
-
-      <p>Price: {product.price}</p>
-      <p>Category: {category}</p>
-      <p>Description: {product.desc}</p>
-
-      <Button onClick={() => props.addToCart(product.id)}>
-        Add to cart
-      </Button>
-
-      <Link to='/'>
-        <Button>Back to shop</Button>
-      </Link>
-    </Screen>
+    <ProductsDetailsView
+      product={product}
+      category={category}
+      addToCart={()=> props.addToCart(product.id)}/>
     )
 }
 
---
+++ src/containers/ProductDetails/ProductDetailsView.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Screen from '../../components/Screen'
+import Button from '../../components/Button'
+import { Link } from 'react-router-dom'
+
+const ProductDetailsView = (props) => {
+    return (
+      <Screen>
+        <h3>{props.product.name}</h3>
+
+        <p>Price: {props.product.price}</p>
+        <p>Category: {props.category}</p>
+        <p>Description: {props.product.desc}</p>
+        {console.log(props.product.id)}
+        <Button onClick={() => props.addToCart(props.product.id)}>
+          Add to cart
+        </Button>
+
+        <Link to='/'>
+          <Button>Back to shop</Button>
+        </Link>
+      </Screen>
+    )
+}
+
+export default ProductDetailsView
